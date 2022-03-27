f4b73d9ff6bd1ac3552c47b7cfe7cc9d4a4a718c
thomasHorvath <116713@fhwn.ac.at>
2022-03-27 22:57:02 +0200

    Add Map to frontend
    Add Map to frontend

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -5,6 +5,7 @@ function Navbar() {
       <div className='content'>
         <button><a>Map</a></button>
         <button><a>Character</a></button>
+        <button><a>Logo :)</a></button>
         <button><a>About</a></button>
         <button><a>Login</a></button>
       </div>
