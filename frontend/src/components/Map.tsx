e42e23eb892b258681ab02972947f0a3a888255f
thomasHorvath <116713@fhwn.ac.at>
2022-03-27 23:10:28 +0200

    Change index title
    Change index title

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -3,7 +3,7 @@ import mapImage from '../img/mapv0.5.jpg'
 function Map() {
   return (
     <div>
-         <img src={mapImage} style={{"width":"100%", "height":"50%"}}/>
+         <img src={mapImage} style={{"maxWidth":"50%", "maxHeight":"50%"}}/>
     </div>
   )
 }
