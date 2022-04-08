74b2e075a5a8fe9f0c19f192cb99d53fe84ba579
thomasHorvath <116713@fhwn.ac.at>
2022-04-08 21:15:36 +0200

    Frontpage change styling
    Frontpage change

--- frontend/src/components/page/Home.tsx
+++ frontend/src/components/page/Home.tsx
@@ -1,14 +1,22 @@
 import Navbar from "../Navbar/Navbar"
+import mapImage from '../../img/mapv0.5.jpg'
 import { Link, useNavigate } from "react-router-dom";
 function Home() {
   return (
     <div>
       <Navbar/>
-      <div className="text-center">
-        <h1>Welcome to Middle Earth Maps!</h1>
+      <div className="demo-wrap">
+  <img
+    className="demo-bg"
+    src={mapImage}
+    alt=""
+  />
+  <div className="demo-content text-center">
+   <h1>Welcome to Middle Earth Maps</h1>
         <p>Explore the world of middle earth with our map.</p>
         <Link to="../register" className="btn btn-primary App">Register</Link>
-      </div>
+  </div>
+</div>
     </div>
   )
 }
