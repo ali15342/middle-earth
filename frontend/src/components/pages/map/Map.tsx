7f152756bf2a8d48b2157556a9283992dcb57eaa
tnoori98 <thomas.noori@gmail.com>
2022-06-18 00:50:33 +0200

    map done

--- frontend/src/components/pages/map/Map.tsx
+++ frontend/src/components/pages/map/Map.tsx
@@ -1,41 +1,75 @@
 import {
-    Circle,
     ImageOverlay,
     MapContainer,
-    Marker,
-    Polyline,
-    Popup,
     SVGOverlay,
     TileLayer,
-    useMapEvent,
-    useMapEvents
+    useMapEvent
 } from "react-leaflet";
-import React, { MutableRefObject, useState } from "react";
+import React, { useState } from "react";
 import "leaflet/dist/leaflet.css";
+import { Checkbox } from "@material-ui/core";
 
 function MyComponent() {
-    const [showMarker, setShowMarker] = useState(false);
+    const [showMordor, setShowMordor] = useState(false);
 
     const map = useMapEvent("zoom", (ev) => {
-        setShowMarker(true);
+        setShowMordor(false);
         switch (ev.target.getZoom()) {
             case 1:
                 break;
             case 2:
                 break;
             case 3:
+                setShowMordor(true);
                 break;
             default:
-                console.log("4");
+                setShowMordor(true);
         }
     });
 
-    return null;
+    return showMordor ? (
+        <ImageOverlay
+            bounds={[
+                [-30, 52],
+                [-70, 90]
+            ]}
+            url={"/map/mordor.svg"}
+        />
+    ) : null;
 }
 
 function Map() {
+    const [hobbitRoute, setHobbitRoute] = useState(false);
+    const [dwarfRoute, setDwarfRoute] = useState(false);
+
+    function onHobbitRouteClick() {
+        setHobbitRoute(!hobbitRoute);
+    }
+
+    function callTest() {
+        console.log("mordor clicked2");
+    }
+
+    function onLegolasGimliRouteClick() {
+        setDwarfRoute(!dwarfRoute);
+        document
+            .getElementById("mordorSVG")!
+            .addEventListener("click", function () {
+                console.log("mordor clicked");
+            });
+    }
+
     return (
         <div style={{ width: "800px", height: "500px" }}>
+            <label>
+                Paths of
+                <Checkbox onClick={() => onHobbitRouteClick()} />
+                Frodo & Sam
+            </label>
+            <label>
+                <Checkbox onClick={() => onLegolasGimliRouteClick()} />
+                Legolas & Gimli
+            </label>
             <MapContainer
                 center={[1, 1]}
                 zoom={1}
@@ -45,20 +79,25 @@ function Map() {
                 style={{ height: "100%", width: "100%" }}
             >
                 <MyComponent />
-               <ImageOverlay
-                    bounds={[
-                        [-200, -100],
-                        [100, 100]
-                    ]}
-                    url={"/map/frodo-sam-path.svg"}
-                />
-                <ImageOverlay
-                    bounds={[
-                        [-200, -100],
-                        [100, 100]
-                    ]}
-                    url={"/map/legolas-gimli-path.svg"}
-                />
+
+                {hobbitRoute && (
+                    <ImageOverlay
+                        bounds={[
+                            [-200, -100],
+                            [100, 100]
+                        ]}
+                        url={"/map/frodo-sam-path.svg"}
+                    />
+                )}
+                {dwarfRoute && (
+                    <ImageOverlay
+                        bounds={[
+                            [-200, -100],
+                            [100, 100]
+                        ]}
+                        url={"/map/legolas-gimli-path.svg"}
+                    />
+                )}
                 <TileLayer noWrap={true} url="/tiles/{z}/{x}/{y}.jpg" />
             </MapContainer>
         </div>
