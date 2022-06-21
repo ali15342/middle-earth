eae63cb37ec6217c8e9638652884f0ad3edc5bba
tnoori98 <thomas.noori@gmail.com>
2022-06-21 10:19:38 +0200

    Revert "CSS"
    This reverts commit ae49934bdc5201bc03236a472aff838ed4a83173.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./css/index.css";
+import "./css/index.sss";
 import App from "./App";
 import { Toaster } from "react-hot-toast";
 
